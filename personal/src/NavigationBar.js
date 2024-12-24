50359d6eebe32e772a5ad9cf3c5fd276190ac8bd
ConnerDeFeo <ninjanerozz@gmail.com>
2024-12-23 22:22:29 -0500

    what up

--- personal/src/NavigationBar.js
+++ personal/src/NavigationBar.js
@@ -4,9 +4,9 @@ import { Button, Navbar, NavbarText } from "reactstrap";
 function NavigationBar(){
     return(
         <Navbar color='dark' className='d-flex'>
-          <Button color='light' className='navSpacing'><Link to='/' className='text-dark text-decoration-none'>Home</Link></Button>
-          <NavbarText className='text-light navSpacing'>Conner DeFeo | Rochester,NY | Full-Stack Developer</NavbarText>
-          <Button className='bg-light'><Link to='/contact' className='text-dark text-decoration-none'>Contact</Link></Button>
+          <Button color='light' ><Link to='/' className='text-dark text-decoration-none'>Home</Link></Button>
+          <NavbarText className="text-light">Conner DeFeo | Rochester,NY | Full-Stack Developer</NavbarText>
+          <Button color="light"><Link to='/contact' className='text-dark text-decoration-none'>Contact</Link></Button>
         </Navbar>
     );
 }
