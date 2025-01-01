e13fd387ef53794f8e1e0b394166fcaa01391a89
ConnerDeFeo <ninjanerozz@gmail.com>
2025-01-01 14:09:23 -0500

    homepage now compatible on mobile

--- personal/src/App.js
+++ personal/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, useLocation, useNavigate } from 'react-router-dom'; 
 import Home from './Home';
 import Contact from './Contact';
-import { Button, Col, Container, Navbar, NavbarText, Row } from 'reactstrap';
+import { Button, Col, Container, Navbar, Row } from 'reactstrap';
 import ProjectPage from './ProjectPage';
 
 function Navigation(){
@@ -15,11 +15,8 @@ function Navigation(){
   };
 
   return (
-    <Navbar className='d-flex bg-black'>
+    <Navbar className='d-flex bg-black Navbar'>
       <Button color='light' className="navButton" onClick={() => handleNavigation('/')}>Home</Button>
-      <NavbarText className="text-light">
-        <p>Rochester, NY | Full-Stack Developer</p>
-      </NavbarText>
       <Button color="light" className="navButton" onClick={() => handleNavigation('/contact')}>Contact</Button>
     </Navbar>
   );
@@ -40,7 +37,7 @@ const App = () => {
       </Routes>
       <Container className="mw-100 pTop">
           <Row className="text-center">
-              <Col><p>@2025 Conner DeFeo</p></Col>
+              <Col xs={12} md={4}><p>@2025 Conner DeFeo</p></Col>
               <Col><a href='https://linkedin.com/in/conner-jack-defeo' target='_Blank' rel='noreferrer' className='text-light text-decoration-none'><p className="linkedin d-inline-block">Linkedin</p></a></Col>
               <Col><p onClick={()=>window.scroll({top:0, behavior:'smooth'})} className='backToTop d-inline-block'>BACK TO TOP</p></Col>
           </Row>
